9750fde5333f3b4e96b6c78d363f75a4e5fa743f
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-21 15:45:01 -0700

    equivalent to rm -rf

---
+++ python/HDFS/delete.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python
+from snakebite.client import Client
+
+client = Client('localhost', 9000)
+
+# recurse=True is equivalent to rm -rf so be careful!
+for p in client.delete(['/foo', '/another'], recurse=True):
+    print p
