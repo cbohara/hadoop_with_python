0b7d225f922b4b07f98038bce24bc2ba47acfcea
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-21 15:45:47 -0700

    make additional directories and subdirectories into HDFS

---
+++ python/HDFS/mkdir.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python
+from snakebite.client import Client
+
+client = Client('localhost', 9000)
+
+# specifying create_parent=True is the equivalent of mkdir -p
+for p in client.mkdir(['/foo/bar', '/another'], create_parent=True):
+    print p
