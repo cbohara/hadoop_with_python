2711862525b3e93d9c4ba895165f4a508be5b970
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-21 15:33:18 -0700

    adding notes about snakebite

--- python/HDFS/list_directory.py
+++ python/HDFS/list_directory.py
@@ -1,6 +1,12 @@
 #!/usr/local/bin/python
 from snakebite.client import Client
 
+# this line creates the client connection to the HDFS NameNode
+# NameNode hostname = localhost, NameNode port = 9000
+# these parameters are set in hadoop/conf/core-site.xml under fs.defaultFS
 client = Client('localhost', 9000)
+
+# list the content of the HDFS root directory
+# note that many methods in snakebite returns generators 
 for x in client.ls(['/']):
     print x
