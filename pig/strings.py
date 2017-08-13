cec1c83ad08d44435665082d76fee05172661572
hduser <hduser@charlies-mbp.gateway.pace.com>
2017-08-12 20:04:08 -0700

    keeping used udf

--- pig/strings.py
+++ pig/strings.py
@@ -1,9 +1,5 @@
 from pig_util import outputSchema
 
-@outputSchema('word:chararray')
-def reverse(word):
-    return word[::-1]
-
 @outputSchema('length:int')
 def num_chars(word):
     return len(word)
